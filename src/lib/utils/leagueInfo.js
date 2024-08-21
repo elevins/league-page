5b258775f816cd1f987ba5408ecedf4461a04f79
elevins <spencerlevins@gmail.com>
2024-08-21 16:11:46 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -142,7 +142,7 @@ export const managers = [
     },
     {
       "roster": 6,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-      "managerID": "997351925613957120",  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      "managerID": "997200559243309056",  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "name": "steviejez",
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "SF", // (optional)
@@ -234,7 +234,7 @@ export const managers = [
     },
     {
       "roster": 10,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-      "managerID": "997351925613957120",  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      "managerID": "997351925613957120", // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "name": "caswartz",
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "NYC", // (optional)
