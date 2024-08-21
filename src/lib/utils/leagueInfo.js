3ce4d9375fa7ae0fd64e5f3440486590f9073af0
elevins <spencerlevins@gmail.com>
2024-08-21 16:57:11 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -193,7 +193,7 @@ export const managers = [
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "NYC", // (optional)
       "bio": "Lorem ipsum...",
-      "photo": "/managers/spencer.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/spencer.JPG", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "mia", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
