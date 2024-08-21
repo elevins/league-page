c2de9d054645a361468b9b17c432da6121fcb4df
elevins <spencerlevins@gmail.com>
2024-08-21 15:48:18 -0400

    Update leagueInfo.js
    pic test

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -51,11 +51,11 @@ export const managers = [
     {
       "roster": 2,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID": "994692260442759168",  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-      "name": "NathanVellayan",
+      "name": "Nathan Vellayan",
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "NYC", // (optional)
       "bio": "Lorem ipsum...",
-      "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/nathan.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "was", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
