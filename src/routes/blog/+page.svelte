414b397af261b56301a8858f38d667b8c99f39c9
Spencer Levins <spencerlevins@gmail.com>
2025-10-29 13:23:51 -0400

    revert: blog post title changes due to linking issues

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,13 +1,25 @@
 <script>
     import { Posts } from "$lib/components";
-	import { leagueName } from '$lib/utils/helper';
-	import { pageTitle } from '$lib/stores';
-
-	$pageTitle = `Blog | ${leagueName}`;
 
     export let data;
     const {postsData, queryPage, filterKey, leagueTeamManagersData} = data;
 </script>
+
+<style>
+    #main {
+        position: relative;
+        z-index: 1;
+        display: block;
+        margin: 30px auto;
+		width: 95%;
+		max-width: 1000px;
+        overflow-y: hidden;
+    }
+</style>
+
+<div id="main">
+    <Posts {postsData} {queryPage} {filterKey} {leagueTeamManagersData} />
+</div>
 
 <style>
     #main {
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,19 +1,42 @@
 <!-- src/routes/blog/[slug]/+page.svelte -->
 <script>
     import { FullPost } from "$lib/components";
-	import { leagueName } from '$lib/utils/helper';
-	import { pageTitle } from '$lib/stores';
 
     export let data;
     const {postsData, postID, leagueTeamManagersData} = data;
-
-	if (postsData && postsData.length > 0) {
-		const post = postsData.filter(p => p.sys.id === postID)[0];
-		if (post) {
-			$pageTitle = `${post.fields.title} | ${leagueName}`;
-		}
-	}
 </script>
+
+<style>
+    #main {
+        position: relative;
+        z-index: 1;
+        display: block;
+        margin: 30px auto;
+		width: 95%;
+		max-width: 1000px;
+        overflow-y: hidden;
+    }
+
+    .center {
+        text-align: center;
+        margin-bottom: 2em;
+    }
+
+    .viewAll {
+        text-decoration: none;
+        background-color: #920505;
+        color: #fff;
+        border-radius: 1em;
+        padding: 0.5em 1em;
+    }
+</style>
+
+<div id="main">
+    <FullPost {postsData} {postID} {leagueTeamManagersData} />
+    <div class="center">
+        <a class="viewAll" href="/blog">View More Blog Posts</a>
+    </div>
+</div>
 
 <style>
     #main {
