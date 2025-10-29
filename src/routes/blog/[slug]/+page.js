4adde0a9790acdca627fd78648ebcc4b0f21d6f2
elevins <spencerlevins@gmail.com>
2025-10-29 11:50:58 -0400

    Update +page.js

--- src/routes/blog/[slug]/+page.js
+++ src/routes/blog/[slug]/+page.js
@@ -1,22 +1,16 @@
+
 import { enableBlog, getBlogPosts, getLeagueTeamManagers } from '$lib/utils/helper';
 
-export async function load({ fetch, params }) {
-    if (!enableBlog) return {};
-
+export function load({ fetch, params }) {
+    if(!enableBlog) return false;
+    
     const postID = params.slug;
-
-    const [postsData, leagueTeamManagersData] = await Promise.all([
-        getBlogPosts(fetch),
-        getLeagueTeamManagers()
-    ]);
-
-    const currentPost = postsData.find(p => p.fields.slug === postID);
-    const title = currentPost?.fields?.title || "Blog Post";
+    const postsData = getBlogPosts(fetch);
+    const leagueTeamManagersData = getLeagueTeamManagers();
 
     return {
         postsData,
         postID,
         leagueTeamManagersData,
-        title
     };
 }
