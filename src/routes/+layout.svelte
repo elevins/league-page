80f76c7aef1c019d94d6c92ba7234790a0b14367
elevins <spencerlevins@gmail.com>
2025-10-29 12:16:18 -0400

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,28 @@
     injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
+<svelte:head>
+	<title>Chyones League Page</title>
+
+	<!-- iMessage Rich Preview -->
+	<meta property="og:title" content="NYC + SF Fantasy Football" />
+	<meta property="og:description" content="Custom Sleeper fantasy football league with standings, trades, power rankings, and more!" />
+	<meta property="og:image" content="https://chyones.com/badge.png" />
+	<meta property="og:url" content={$page.url.href} />
+	<meta property="og:type" content="website" />
+	<meta property="og:site_name" content="Chyones />
+
+	<!-- Twitter Card -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="Chyones League Page" />
+	<meta name="twitter:description" content="Custom Sleeper fantasy football league with standings, trades, power rankings, and more!" />
+	<meta name="twitter:image" content="https://chyones.com/badge.png" />
+
+	<!-- Image size -->
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
+</svelte:head>
+
 <main>
     <Nav /> <!-- adds the nav (small and large) -->
   
