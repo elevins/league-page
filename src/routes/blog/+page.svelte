044b9b001ea417a4037a46cdd0bbac4e313dcc60
Spencer Levins <spencerlevins@gmail.com>
2025-10-29 13:25:21 -0400

    fix: remove duplicate style block in blog page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -20,19 +20,3 @@
 <div id="main">
     <Posts {postsData} {queryPage} {filterKey} {leagueTeamManagersData} />
 </div>
-
-<style>
-    #main {
-        position: relative;
-        z-index: 1;
-        display: block;
-        margin: 30px auto;
-		width: 95%;
-		max-width: 1000px;
-        overflow-y: hidden;
-    }
-</style>
-
-<div id="main">
-    <Posts {postsData} {queryPage} {filterKey} {leagueTeamManagersData} />
-</div>
