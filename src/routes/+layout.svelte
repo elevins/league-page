17feefeb29c1f82bc4d698501cdeddc22554d076
elevins <spencerlevins@gmail.com>
2025-10-29 11:53:14 -0400

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,34 +1,16 @@
+<!-- __layout.svelte -->
 <script>
-	import { Nav, Footer } from "$lib/components";
-	import { dev } from '$app/environment';
-	import { injectAnalytics } from '@vercel/analytics/sveltekit';
-	import { page } from '$app/stores';
-
-	injectAnalytics({ mode: dev ? 'development' : 'production' });
-
-	const leagueName = "Chyones Fantasy League";
-	const imageUrl = "https://chyones.com/badge.png";
-
-	$: title = $page.data.title || "League Page";
-	$: fullTitle = `${title} | ${leagueName}`;
+	import { Nav, Footer } from "$lib/components"
+    import { dev } from '$app/environment';
+    import { injectAnalytics } from '@vercel/analytics/sveltekit';
+ 
+    injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
-<svelte:head>
-	<title>{fullTitle}</title>
-	<meta property="og:title" content={fullTitle} />
-	<meta property="og:description" content="Custom Sleeper league with standings, trades, power rankings, and more!" />
-	<meta property="og:image" content={imageUrl} />
-	<meta property="og:url" content={$page.url.href} />
-	<meta property="og:type" content="website" />
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:title" content={fullTitle} />
-	<meta name="twitter:image" content={imageUrl} />
-	<meta property="og:image:width" content="1200" />
-	<meta property="og:image:height" content="630" />
-</svelte:head>
-
 <main>
-	<Nav />
-	<slot />
-	<Footer />
+    <Nav /> <!-- adds the nav (small and large) -->
+  
+    <slot />
+
+    <Footer /> <!-- adds the footer -->
 </main>
